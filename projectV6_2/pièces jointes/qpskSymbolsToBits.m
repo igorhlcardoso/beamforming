9afd1b6113ce3757b818b05115a2bbc18d811fc7
igorhlcardoso <136521779+igorhlcardoso@users.noreply.github.com>
2026-06-12 11:47:21 +0200

    Delete projectV6_2 directory

--- projectV6_2/pièces jointes/qpskSymbolsToBits.m
+++
@@ -1,9 +0,0 @@
-function bits = qpskSymbolsToBits(sym, p)
-
-demod = pskdemod(sym, p.M, p.phaseOffset, 'gray');
-
-bitsMat = de2bi(demod, p.k, 'left-msb').';
-
-bits = bitsMat(:);
-
-end
--- projectV6_2/pièces jointes/txChain.m
+++
@@ -1,9 +0,0 @@
-function [txBits, txSym, txWave] = txChain(dataBits, preambleBits, p, txFilter)
-
-txBits = [preambleBits(:); dataBits(:)];
-
-txSym = qpskBitsToSymbols(txBits, p);
-
-txWave = upfirdn(txSym, txFilter, p.sps, 1);
-
-end
